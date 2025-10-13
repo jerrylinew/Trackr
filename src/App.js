81f4783f55635e76b1a5eb48a2895e6ddc616449
Alex Tong <alextong1272@gmail.com>
2025-10-13 22:56:41 +0000

    updated app and added shop component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Codes from "./components/Codes";
 import Scan from "./components/Scan";
 import Location from "./components/Location";
-
-
+import Home from "./components/Home";
+import Shop from "./components/Shop";
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Codes />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/codes" element={<Codes />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/:code" element={<Scan />} />
         <Route path="/:code/location" element={<Location />} />
       </Routes>
