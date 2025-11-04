c99657ea9d096c223c9ce65c449a10e423f45022
Alex Tong <alextong1272@gmail.com>
2025-11-04 03:06:04 +0000

    changed code button to print full grid of codes, removed bounty functionality for now

--- src/components/Codes.js
+++ src/components/Codes.js
@@ -5,11 +5,19 @@ import { QRCodeCanvas } from "qrcode.react";
 export default function Codes() {
   const [codes, setCodes] = useState([]);
 
+  const gridSize = 16;
+
   async function handleSubmit(isBounty) {
     try {
-      const { code } = await addCode(isBounty);
-      const url = `${window.location.origin}/${code}`;
-      setCodes((prev) => [...prev, { code, url }]);
+      const promises = Array.from({ length: gridSize }, () =>
+        addCode(isBounty)
+      );
+      const results = await Promise.all(promises);
+      const newCodes = results.map(({ code }) => ({
+        code,
+        url: `${window.location.origin}/${code}`,
+      }));
+      setCodes((prev) => [...prev, ...newCodes]);
     } catch (err) {
       console.error("Failed to add code:", err);
     }
@@ -20,16 +28,60 @@ export default function Codes() {
       <button className="bigButton" onClick={() => handleSubmit(false)}>
         No Bounty
       </button>
-      <button className="bigBountyButton" onClick={() => handleSubmit(true)}>
-        Bounty
-      </button>
-
-      <div>
+      <button className="bigBountyButton">Bounty</button>{" "}
+      {/* add this for bounty onClick={() => handleSubmit(true)}}*/}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: "1rem",
+          marginTop: "2rem",
+        }}
+      >
         {codes.map(({ code, url }) => (
-          <div key={code} style={{ marginTop: "1rem" }}>
-            <QRCodeCanvas value={url} size={128} />
+          <div
+            key={code}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {/* Text at the top */}
+            <div
+              style={{
+                marginBottom: "0.5rem",
+                fontSize: "25px",
+                fontFamily: "Inter, sans-serif",
+                display: "flex",
+                alignItems: "center",
+                gap: "0.2rem",
+              }}
+            >
+              <img src="trackrLogo.png" width="22px" />
+              Trackr
+            </div>
 
-            <p>Code: {code}</p>
+            <QRCodeCanvas value={url} size={128} />
+            <div
+              style={{
+                marginTop: "0.5rem",
+                fontFamily: "Inter, sans-serif",
+                display: "flex",
+              }}
+            >
+              Found This Item?
+            </div>
+            <div
+              style={{
+                marginTop: "0.2rem",
+                fontFamily: "Inter, sans-serif",
+                display: "flex",
+                fontSize: "12px",
+              }}
+            >
+              Scan to notify owner
+            </div>
           </div>
         ))}
       </div>
