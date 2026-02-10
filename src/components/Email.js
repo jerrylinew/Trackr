0b4d6952f50cb1cfe07b215a08d3699c488f9856
90Entergetial <90Entergetial@gmail.com>
2026-02-09 19:15:07 -0800

    this is a commit message

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,7 +1,21 @@
 export default function Email() {
   return (
     <>
-      <h1>SHO</h1>
+      <div className="emailplacething">
+        <div className="inputtheemail">
+          <h1>Send Code to Email</h1>
+          <input
+            name="thegmeil"
+            type="email"
+            placeholder="Your Gmail"
+            className="email-input"
+          />
+          <p className="newline">.</p>
+          <button type="submit" className="email-email-button">
+            Send Code to Email
+          </button>
+        </div>
+      </div>
     </>
   );
 }
