52139dc9b18185eaf23d2adc2bfdc0d128dccc97
90Entergetial <90Entergetial@gmail.com>
2026-02-23 18:34:27 -0800

    item is being shown on the item shown page

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,6 +1,17 @@
 import { useSearchParams } from 'react-router-dom';
 import { IDGmail } from '../services/database';
 import { useState } from 'react';
+
+function itemLister(Name, Objname, Desc){
+  return(
+    <div className="itemlister">
+    <p>Your Name: {Name}</p>
+    <p>Object Name: {Objname}</p>
+    <p>Found Message: {Desc}</p>
+    </div>
+  );
+}
+
 export default function Items() {
   const [itemlist, setItemlist] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
@@ -16,18 +27,22 @@ export default function Items() {
   }
   async function getTheItems(){
     setItemlist(await IDGmail(email));
+    
   }
   if(email != "" && itemlist.length == 0){
     getTheItems();
   }
   console.log(itemlist);
+  const itemlistshower = (itemlist.slice(1)).map((x) => itemLister(x[0],x[1],x[2]));
 
   
 
   return (
     <>
       <h1>Nice items {itemlist[0]}</h1>
-      <p></p>
+      <p>You have {itemlist.length-1 } items!
+      </p>
+      {itemlistshower}
     </>
   );
 }
